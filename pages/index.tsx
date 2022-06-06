4964c3176f55d14c45962880ae2ab42ae1544073
Willian Ferreira <williannfferreira@gmail.com>
2022-06-06 00:11:10 -0300

    feat: add container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  font-weight: bold;
+`;
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    setTimeout(() => {
-      console.log('Env', process.env.NEXT_PUBLIC_TITLE)
-    }, 5000);
-  }, [])
   return (
-    <div>{process.env.NEXT_PUBLIC_TITLE}</div>
+    <Container>
+      <h1>{process.env.NEXT_PUBLIC_TITLE}</h1>
+    </Container>
   )
 }
 
