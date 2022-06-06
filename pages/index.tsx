da2f6d3e0f695990d72bd9fe07e8577e52677eb8
Willian Ferreira <williannfferreira@gmail.com>
2022-06-05 23:39:14 -0300

    chore: init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
 
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    console.log('Env', process.env)
+  }, [])
   return (
     <div>{process.env.TITLE || 'Texto padrão'}</div>
   )
