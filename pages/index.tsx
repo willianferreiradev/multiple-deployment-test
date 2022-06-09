90d92f142f1da30e1ca2f64e8a939d3bab035cc1
Willian Ferreira <williannfferreira@gmail.com>
2022-06-08 21:59:06 -0300

    feat: add more texts to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size: 30px;
@@ -13,12 +14,34 @@ const Container = styled.div`
 
   background-color: #38A3A5;
   color: #C7F9CC;
+  text-align: center;
+
+  > h1 {
+    font-size: 2.5rem;
+    margin-bottom: 5px;
+  }
+
+  > p {
+    max-width: 50%;
+    margin-top: 25px;
+
+    @media only screen and (max-width: 1600px) {
+      max-width: 80%;
+    }
+
+    @media only screen and (max-width: 800px) {
+      max-width: 95%;
+    }
+  }
 `;
 
 const Home: NextPage = () => {
   return (
     <Container>
       <h1>{process.env.NEXT_PUBLIC_TITLE}</h1>
+      <p>this is a simple project to demonstrate multiple deploys on vercel.</p>
+      <p>
+        in the environment variables I look for the title that is up there. In this case I have the same project with multiple deploys and I can change values ​​per domain</p>
     </Container>
   )
 }
